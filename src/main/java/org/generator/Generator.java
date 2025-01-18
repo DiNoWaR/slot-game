cb65dfc3ef728b65b69c2e62e3b09323fa15b54e
denisvasilev <dinowar1@gmail.com>
2025-01-18 11:23:13 +0400

    Fix matrix generator

--- src/main/java/org/generator/Generator.java
+++ src/main/java/org/generator/Generator.java
@@ -5,104 +5,67 @@ import org.config.model.GameConfig;
 import java.util.*;
 
 public class Generator {
+    private static final int BONUS_SYMBOL_POSSIBILITY = 10;
+    private final Random random = new Random();
+
     public String[][] generateMatrix(GameConfig config) {
-        var random = new Random();
         var rows = config.getRows();
         var columns = config.getColumns();
         var matrix = new String[rows][columns];
 
         var standardSymbols = config.getProbabilities().getStandardSymbols();
+        var bonusSymbols = config.getProbabilities().getBonusSymbols().getSymbols();
 
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < columns; col++) {
-                matrix[row][col] = getRandomSymbol(standardSymbols, random);
+                if (isBonus(random)) {
+                    matrix[row][col] = getRandomBonusSymbol(bonusSymbols);
+                } else {
+                    matrix[row][col] = getRandomStandardSymbol(standardSymbols, row, col);
+                }
             }
         }
         return matrix;
     }
 
-    public Map<String, List<String>> checkWinningCombinations(String[][] matrix, GameConfig config) {
-        Map<String, List<String>> appliedCombinations = new HashMap<>();
+    private String getRandomStandardSymbol(List<GameConfig.StandardSymbol> symbols, int row, int col) {
+        for (GameConfig.StandardSymbol symbolConfig : symbols) {
+            if (symbolConfig.getRow() == row && symbolConfig.getColumn() == col) {
+                var symbolWeights = symbolConfig.getSymbols();
+                var totalWeight = symbolWeights.values().stream().mapToInt(Integer::intValue).sum();
+                var randomValue = random.nextInt(totalWeight);
 
-        for (Map.Entry<String, GameConfig.WinCombination> entry : config.getWinCombinations().entrySet()) {
-            GameConfig.WinCombination combination = entry.getValue();
-
-            if (combination.getWhen().equals("same_symbols")) {
-                String symbol = findMatchingSymbols(matrix, combination.getCount());
-                if (symbol != null) {
-                    appliedCombinations.putIfAbsent(symbol, new ArrayList<>());
-                    appliedCombinations.get(symbol).add(entry.getKey());
-                }
-            }
-        }
-        return appliedCombinations;
-    }
-
-    public double calculateReward(double betAmount, Map<String, List<String>> winningCombinations, GameConfig config) {
-        double reward = 0;
-
-        for (Map.Entry<String, List<String>> entry : winningCombinations.entrySet()) {
-            String symbol = entry.getKey();
-            List<String> combinations = entry.getValue();
-
-            GameConfig.Symbol configSymbol = config.getSymbols().get(symbol);
-            double symbolReward = betAmount * configSymbol.getRewardMultiplier();
-
-            for (String combination : combinations) {
-                GameConfig.WinCombination winCombination = config.getWinCombinations().get(combination);
-                symbolReward *= winCombination.getRewardMultiplier();
-            }
-
-            reward += symbolReward;
-        }
-        return reward;
-    }
-
-    public double applyBonus(double reward, String[][] matrix, GameConfig config) {
-        for (String[] row : matrix) {
-            for (String cell : row) {
-                GameConfig.Symbol symbol = config.getSymbols().get(cell);
-                if (symbol != null && symbol.getType().equals("bonus")) {
-                    if (symbol.getImpact().equals("multiply_reward")) {
-                        reward *= symbol.getRewardMultiplier();
-                    } else if (symbol.getImpact().equals("extra_bonus")) {
-                        reward += symbol.getExtra();
+                var cumulativeWeight = 0;
+                for (Map.Entry<String, Integer> entry : symbolWeights.entrySet()) {
+                    cumulativeWeight += entry.getValue();
+                    if (randomValue < cumulativeWeight) {
+                        return entry.getKey();
                     }
                 }
             }
         }
-        return reward;
-    }
-
-    private String getRandomSymbol(List<GameConfig.StandardSymbol> symbols, Random random) {
-        int totalWeight = symbols.stream().mapToInt(s -> s.getSymbols().values().stream().mapToInt(Integer::intValue).sum()).sum();
-        int randomValue = random.nextInt(totalWeight);
-
-        int cumulativeWeight = 0;
-        for (GameConfig.StandardSymbol symbol : symbols) {
-            for (Map.Entry<String, Integer> entry : symbol.getSymbols().entrySet()) {
-                cumulativeWeight += entry.getValue();
-                if (randomValue < cumulativeWeight) {
-                    return entry.getKey();
-                }
-            }
-        }
         return null;
     }
 
-    private String findMatchingSymbols(String[][] matrix, int count) {
-        Map<String, Integer> symbolCounts = new HashMap<>();
-        for (String[] row : matrix) {
-            for (String cell : row) {
-                symbolCounts.put(cell, symbolCounts.getOrDefault(cell, 0) + 1);
-            }
+    private String getRandomBonusSymbol(Map<String, Integer> bonusSymbols) {
+        var totalWeight = bonusSymbols.values().stream().mapToInt(Integer::intValue).sum();
+        if (totalWeight == 0) {
+            return "MISS";
         }
 
-        for (Map.Entry<String, Integer> entry : symbolCounts.entrySet()) {
-            if (entry.getValue() >= count) {
+        var randomValue = random.nextInt(totalWeight);
+        var cumulativeWeight = 0;
+
+        for (Map.Entry<String, Integer> entry : bonusSymbols.entrySet()) {
+            cumulativeWeight += entry.getValue();
+            if (randomValue < cumulativeWeight) {
                 return entry.getKey();
             }
         }
-        return null;
+        return "MISS";
+    }
+
+    private boolean isBonus(Random random) {
+        return random.nextInt(100) < BONUS_SYMBOL_POSSIBILITY;
     }
 }
--- src/main/java/org/main/Main.java
+++ src/main/java/org/main/Main.java
@@ -1,30 +1,27 @@
 package org.main;
 
 import org.apache.commons.cli.*;
-import org.config.model.GameConfig;
 import org.config.parser.Parser;
 import org.generator.Generator;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Options options = new Options();
+        var options = new Options();
 
         options.addOption("c", "config", true, "Path to the configuration file");
         options.addOption("b", "betting-amount", true, "Betting amount");
 
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
+        var parser = new DefaultParser();
+        var formatter = new HelpFormatter();
 
         try {
-            CommandLine cmd = parser.parse(options, args);
+            var cmd = parser.parse(options, args);
 
-            String configPath = cmd.getOptionValue("config");
-            String bettingAmountStr = cmd.getOptionValue("betting-amount");
+            var configPath = cmd.getOptionValue("config");
+            var bettingAmountStr = cmd.getOptionValue("betting-amount");
 
             if (configPath == null || bettingAmountStr == null) {
                 throw new ParseException("Both --config and --betting-amount are required");
@@ -37,20 +34,14 @@ public class Main {
                 throw new ParseException("Betting amount must be a valid number");
             }
 
-            GameConfig config = Parser.parseConfig(configPath);
+            var config = Parser.parseConfig(configPath);
             var generator = new Generator();
-            String[][] matrix = generator.generateMatrix(config);
+            var matrix = generator.generateMatrix(config);
 
             System.out.println("Generated Matrix:");
             for (String[] row : matrix) {
                 System.out.println(Arrays.toString(row));
             }
-
-            Map<String, List<String>> winningCombinations = generator.checkWinningCombinations(matrix, config);
-            double reward = generator.calculateReward(bettingAmount, winningCombinations, config);
-            reward = generator.applyBonus(reward, matrix, config);
-
-            System.out.println("Final Reward: " + reward);
 
         } catch (ParseException e) {
             System.err.println("Error parsing arguments: " + e.getMessage());
